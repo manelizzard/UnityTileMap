6ccf9f1a030045e4265d4a2d1a3f8d08e531db48
manelizzard <manelizzard@gmail.com>
2016-09-16 17:00:55 +0200

    Initial commit

---
+++ Assets/Editor/TGMapInspector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TGMap))]
+public class TGMapInspector : Editor {
+
+	public override void OnInspectorGUI () {
+		base.OnInspectorGUI ();
+
+		if (GUILayout.Button ("Regenerate")) {
+			TGMap tileMap = (TGMap)target;
+			tileMap.BuildMesh ();
+		}
+	}
+}
---
+++ Assets/Tile Data/TDTile.cs
@@ -0,0 +1,11 @@
+﻿public class TDTile {
+
+	public enum TYPE {
+		OCEAN,
+		GRASS,
+		PLAIN,
+		MOUNTAIN
+	}
+
+	public TYPE type = TYPE.OCEAN;
+}
